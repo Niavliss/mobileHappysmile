36bfe2b7569e45a7cc8fd299a1fac54f2444adda
lejvincent <lej.vincent26@gmail.com>
2018-06-07 14:01:45 +0200

    API data on Posts Screen

--- src/components/PostResume/index.js
+++ src/components/PostResume/index.js
@@ -4,6 +4,8 @@ import {
     View,
     ScrollView,
     TouchableOpacity,
+    ActivityIndicator,
+    FlatList,
 } from 'react-native';
 import stylePost from './styles';
 
@@ -12,71 +14,55 @@ export default class PostResume extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {isLoading: true}
+    }
+
+    componentDidMount() {
+        return fetch('https://www.happy-smile.fr/api/post')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                }, function () {
+
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
         return (
-            <ScrollView>
-
-                    <View style={stylePost.box}>
-                        <TouchableOpacity onPress={() => this.props.navigation.navigate('Post')}>
-                        <Text style={stylePost.title}> Titre </Text>
-                        <Text> Nom de l'auteur </Text>
-                        <Text> Contenu: du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du
-                            bla
-                            bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                        </Text>
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={stylePost.box}>
-                        <Text style={stylePost.title}> Titre </Text>
-                        <Text> Nom de l'auteur </Text>
-                        <Text> Contenu: du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du
-                            bla
-                            bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                        </Text>
-                    </View>
-                    <View style={stylePost.box}>
-                        <Text style={stylePost.title}> Titre </Text>
-                        <Text> Nom de l'auteur </Text>
-                        <Text> Contenu: du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du
-                            bla
-                            bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                        </Text>
-                    </View>
-                    <View style={stylePost.box}>
-                        <Text style={stylePost.title}> Titre </Text>
-                        <Text> Nom de l'auteur </Text>
-                        <Text> Contenu: du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du
-                            bla
-                            bla,
-                            du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,du bla bla,
-                        </Text>
-                    </View>
-            </ScrollView>
-
-
-    )
+            <View>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({item}) =>
+                        <ScrollView style={stylePost.box}>
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Post')}>
+                                <Text style={stylePost.title}>
+                                    {item.title}
+                                </Text>
+                                <Text>
+                                    Par {item.user.pseudo}
+                                </Text>
+                                <Text>
+                                    {item.content}
+                                </Text>
+                            </TouchableOpacity>
+                        </ScrollView>
+                    }
+                    keyExtractor={(item, index) => index}
+                />
+            </View>
+        )
     }
-    }
+}
